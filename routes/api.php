e03b3daa59b3b2edb1512809ed18319d69ffc0a2
Fidd <jefffdat@gmail.com>
2020-12-15 15:02:07 +0100

    define role

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//define('ASSISTANT', 'ASSISTANT');
-//define('ADMIN', 'ADMIN');
+define('ASSISTANT', 'ASSISTANT');
+define('ADMIN', 'ADMIN');
 
-//define('AUTH_TOKEN', 'user-auth-token');
+define('AUTH_TOKEN', 'user-auth-token');
 
 
 
